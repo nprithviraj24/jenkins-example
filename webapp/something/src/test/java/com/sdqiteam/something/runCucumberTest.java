fdfb085c879e4b7dd66fbd44fc31ad2d89beffb8
rounakrajsurana <rounakraj6@gmail.com>
2018-04-23 09:13:37 +0700

    Cucumber

---
+++ webapp/something/src/test/java/com/sdqiteam/something/runCucumberTest.java
@@ -0,0 +1,14 @@
+package com.sdqiteam.something;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java")
+
+
+public class runCucumberTest {
+
+}
